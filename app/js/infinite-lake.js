1a8ec9c6895f3dd1c254d06e50f77a0995a5cb54
Michael J Erwin <michaeljameserwin@gmail.com>
2015-08-05 03:48:40 +0000

    Updated script tags in index.html

--- app/js/infinite-lake.js
+++ app/js/infinite-lake.js
@@ -1,3 +1,21 @@
-var app = angular.module('infiniteLake', ['app.Services', 'app.Controllers']);
+var app = angular.module('infiniteLake', ['ui.router', 'app.Services', 'app.Controllers']);
 
-
+app.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise("/");
+    
+    $stateProvider
+        .state('index', {
+            url: "/",
+            template: ''
+        })
+        .state('fastWay', {
+            url: "/fast",
+            templateUrl: "_fast.html",
+            controller: 'fastWay'
+        })
+        .state('slowWay', {
+            url: "/slow",
+            templateUrl: "_slow.html",
+            controller: 'slowWay'
+        });
+});
--- app/js/services.js
+++ app/js/services.js
@@ -1,13 +1,13 @@
 angular.module('app.Services', []).service('stepsService', function(){
-    var fastSteps = ['step 1', 'step 2', 'step 3'],
-        slowSteps = ['step 1', 'step 2', 'step 3'],
-        resultStr = '';
+    var fastSteps = [],
+        slowSteps = [],
+        resultObj = {message: "Nothing calculated yet!", success: false};
     init();
     
     function init(){
-        fastSteps = ['step 1', 'step 2', 'step 3'];
-        slowSteps = ['step 1', 'step 2', 'step 3'];
-        result = '';
+        fastSteps = [];
+        slowSteps = [];
+        resultObj = {message: "Nothing calculated yet!", success: false};
     }
 
     var getFastSteps=function(){
@@ -20,33 +20,52 @@ angular.module('app.Services', []).service('stepsService', function(){
     
     var calcSteps = function(target, bucket1, bucket2) {
         console.log("You want " + target + "L of water from a " + bucket1 + "L bucket and a " + bucket2 + "L bucket?");
+        
+        
+        
+        if(!target || !bucket1 || !bucket2){
+            resultObj.success = false;
+            if(!target){
+                resultObj.message = "If you didn't want any water, you could have just said so!";
+            } else {
+                resultObj.message = "You need two buckets that will hold water--no more, no less!";
+            }
+            return;
+        }
+        
+        
         var stepsObject = {};
         stepsObject = calculateSteps(target, bucket1, bucket2);
-        console.dir("from steps service: " + stepsObject.algorithms[0].count);
-        
+
         if(stepsObject.success === false){
-            resultStr = stepsObject.result;
+            resultObj.message = stepsObject.message;
+            resultObj.success = false;
         } else {
             var alg1 = stepsObject.algorithms[0],
                 alg2 = stepsObject.algorithms[1];
                 
+            resultObj.success = true;
+                
             if(alg1.count <= alg2.count){
                 fastSteps = alg1.steps;
-                resultStr = " in the " + alg1.tbucket + " bucket!";
+                resultObj.message = " in the " + alg1.tbucket + " bucket!";
                 slowSteps = alg2.steps;
+                resultObj.altMessage = " in the " + alg2.tbucket + " bucket!";
             } else {
                 fastSteps = alg2.steps;
-                resultStr = " in the " + alg2.tbucket + " bucket!";
+                resultObj.message = " in the " + alg2.tbucket + " bucket!";
                 slowSteps = alg1.steps;
+                resultObj.altMessage = " in the " + alg2.tbucket + " bucket!";
             }
             
-            resultStr = stepsObject.result + resultStr;
+            resultObj.message = stepsObject.message + resultObj.message;
+            resultObj.altMessage = stepsObject.message + resultObj.altMessage;
         }
         
     };
     
     var result = function(){
-        return resultStr;
+        return resultObj;
     };
 
     return{
